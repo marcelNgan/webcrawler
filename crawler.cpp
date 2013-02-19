8c8319b6c6f153ea0def0cca5f8e9b4e92775bf3
Marcel Ngan <marcel@marcel-VirtualBox.(none)>
2013-02-19 23:20:34 +0000

    the links will all be written into a txt file

--- crawler.cpp
+++ crawler.cpp
@@ -6,11 +6,12 @@
 #include <string>
 #include <list>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "simple.cpp"
 
 list<string> extractor(string url);
-void linksprinter (list<string> links);
+void linksprinter (list<string> links, string url);
 
 int main(int argc, char *argv[])
 {
@@ -23,25 +24,44 @@ int main(int argc, char *argv[])
         cin >> url;
         cout << "Crawling: " << url << endl;
         links = extractor(url);
-        linksprinter(links);
+        linksprinter(links, url);
         cout << "Do you wish to crawl another site? (Y|N)";
         cin >> cont;
     }
     return 1;
 }
 
-void linksprinter (list<string> links)
+void linksprinter (list<string> links, string url)
 {
     // if the list is empty...
     if (!links.size())
     {
         cerr << "No links found!" << endl;
-        return;
+    } else
+    {
+        ofstream crawlFile;
+        crawlFile.open("crawl.txt", ios::app);
+        cout << "Written to crawl.txt" << endl; 
+        crawlFile << "There are " << links.size() << " links for " << url << ":"  << endl;
+ 
+        // iterate the links list
+        for (list<string>::iterator it = links.begin(); it != links.end(); it++)
+            crawlFile << *it << endl;
+        crawlFile << endl ;
+        crawlFile.close();
     }
- 
-    cout << "There are " << links.size() << " links:" << endl;
- 
-    // iterate the links list
-    for (list<string>::iterator it = links.begin(); it != links.end(); it++)
-        cout << *it << endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
